df457cc3664d96fc9aebbc19589a862ecede6726
Shobit Beltangdy <ssb402@nyu.edu>
2013-04-30 01:09:07 -0400

    Artist Top Songs

--- soundgarden/soundgarden.py
+++ soundgarden/soundgarden.py
@@ -15,7 +15,7 @@ def about():
 @app.route('/energy', methods=['POST'])
 def energy():
 	artist = request.form.get('artist').title()
-	conn = sqlite3.connect('/home/shobit/development/big-data-project/artist_energy.db')
+	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/artist_energy.db')
 	c = conn.execute("SELECT * FROM artistenergy WHERE artist_name = '" + artist + "'")
 	l = c.fetchone()
 	return jsonify(results = l)
@@ -23,7 +23,7 @@ def energy():
 @app.route('/similar', methods=['POST'])
 def similar():
 	artist = request.form.get('artist').lower()
-	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/mbox/similar_artists.db')
+	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/similar_artists.db')
 	c = conn.execute("select * from similar where artist_name='" + artist + "' ORDER BY CAST(value as float) DESC")
 	l = c.fetchall()
 	return jsonify(similars = l)
@@ -31,7 +31,7 @@ def similar():
 @app.route('/listens', methods=['POST'])
 def listens():
 	artist = request.form.get('artist').lower()
-	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/mbox/top_artists.db')
+	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/top_artists.db')
 	c = conn.execute("SELECT * FROM charts WHERE artist_name = '" + artist + "'");
 	listens = c.fetchone()
 	return jsonify(listens = listens)
@@ -39,7 +39,7 @@ def listens():
 @app.route('/geo', methods=['POST'])
 def geo():
 	artist = request.form.get('artist').title()
-	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/lastfm-dataset-1K/output/geo.db')
+	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/geo.db')
 	c = conn.execute("SELECT country, value FROM geodist where artist_name = '" + artist + "' ORDER BY CAST(value AS INTEGER) DESC")
 	l = c.fetchall()
 	return jsonify(geodata = l)
@@ -47,17 +47,34 @@ def geo():
 @app.route('/demographics', methods=['POST'])
 def demographics():
 	artist = request.form.get('artist').title()
-	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/lastfm-dataset-1K/demo/output/demo.db')
+	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/demo.db')
 	c = conn.execute("SELECT gender, value FROM demo WHERE artist_name='" + artist + "'")
 	l = c.fetchall()
 	return jsonify(d = l)
 @app.route('/topartists', methods=['POST'])
 def topartists():
-	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/mbox/top_artists.db')
+	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/top_artists.db')
 	c = conn.execute("SELECT artist_name, hits FROM charts ORDER BY CAST(hits as integer) DESC LIMIT 50");
-	tops = c.fetchall()
-	return jsonify(top = tops)
+	temp = c.fetchall()
+	topsongs = []
+	for t in temp:
+		m = []
+		m.append(t[0].title())
+		m.append(t[1])
+		topsongs.append(m)
+	print topsongs
+	conn2 = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/song_frequency.db')
+	c2 = conn2.execute("SELECT song_name, artist_name, frequency FROM song_frequency ORDER BY CAST(frequency as INTEGER) DESC LIMIT 50");
+	topartists = c2.fetchall()
+	return jsonify(top = topsongs, artists = topartists)
 
-	
+@app.route('/artisttopsongs', methods=['POST'])
+def artisttopsongs():
+	artist = request.form.get('artist').title()
+	conn = sqlite3.connect('/home/shobit/development/big-data-project/subsets/databases/song_frequency.db')
+	c = conn.execute("SELECT song_name FROM song_frequency WHERE artist_name='" + artist + "' ORDER BY CAST(frequency as INTEGER) DESC LIMIT 10")
+	topsongs = c.fetchall();
+	return jsonify(topsongs = topsongs)
+
 if __name__ == "__main__":
 	app.run(debug=True)
